9d88d9fa96ff3f02b289e70619bc1e7f17996df3
thenabilarta <thenabilarta@gmail.com>
2020-11-01 22:45:30 +0700

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css"
+    href="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css" />
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+  <link rel="stylesheet" href="css/app.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#" id="logo">Mayopentrip</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link" href="#">Beranda</a>
+          <a class="nav-item nav-link" href="#">Liburan</a>
+          <a class="nav-item nav-link" href="#">Blog</a>
+          <a class="nav-item nav-link" href="#">Corporate</a>
+          <a class="nav-item nav-link" href="#">Bantuan</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+  @yield('container')
+
+  <div class="container footer">
+    2014 - 2020 Mayopentrip &copy;
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous">
+  </script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+  <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+  </script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+  </script>
+  <script src="js/index.js"></script>
+</body>
+
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,397 @@
+@extends('layouts.app')
+
+@section('container')
+<div class="container py-4">
+  <div data-skyscanner-widget="SearchWidget" data-destination-name="'Edinburgh'" data-origin-name="'Jakarta'"
+    data-button-text-size="1">
+  </div>
+</div>
+
+<div class="container py-3">
+  <div class="row">
+    <div class="col d-flex align-items-center justify-content-center p-3">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="img/destination/inggris.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Inggris Raya</h5>
+          <p class="card-text">
+            Pesona klasik dataran Inggris dan Irlandia
+          </p>
+          <a href="#" class="btn btn-info">Pelajari</a>
+          <a href="#" class="btn btn-primary">Blog</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col d-flex align-items-center justify-content-center p-3">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="img/destination/eropabarat.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Eropa Barat</h5>
+          <p class="card-text">
+            Deretan negara wajib dikunjungi seperti swiss, italia, jerman, dan prancis.
+          </p>
+          <a href="#" class="btn btn-info">Pelajari</a>
+          <a href="#" class="btn btn-primary">Blog</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col d-flex align-items-center justify-content-center p-3">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="img/destination/amerika.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Amerika Serikat</h5>
+          <p class="card-text">
+            Dua ujung terbaik Amerika, California dan New York!
+          </p>
+          <a href="#" class="btn btn-info">Pelajari</a>
+          <a href="#" class="btn btn-primary">Blog</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col d-flex align-items-center justify-content-center p-3">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="img/destination/kanada.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Kanada</h5>
+          <p class="card-text">
+            Suasana alam asri Kanada berpadu dengan kota-kota maju seperti Vancouver dan Toronto.
+          </p>
+          <a href="#" class="btn btn-info">Pelajari</a>
+          <a href="#" class="btn btn-primary">Blog</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="items">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>bu may sebagai tpur leader sangat kopperatif selalu bertanya ke peserta kita mau nya bagaimana dan
+            seperti apa tidak memaksakan kehendak.. dan soo pasti harga nya sangat murcee.. bisa dibandingkan dengan
+            tour2 yg lain.. ahhhh ayooo kita jalan2 lagi bu.. tak sabar menanti trip2 ibu selanjutnya😘😘😘😘😘</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/ivo.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Dr ivo fitrian SpOG</h4>
+              <p class="cust-profession">Dokter</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Berkesan sekali dengan perjalanan mayopentrip, selain terjangkau, destinasi bisa di sesuaikan dan kita
+            nyaman walau bawa orang tua yang sudah sepuh... maju terus yaa</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/dumasari.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Dumasari</h4>
+              <p class="cust-profession">Pegawai BUMN</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Pernah ikut May Open Trip ke Cina di Maret 2018 dan UK-Ireland di Agustus 2019. Asik dan santuy. Moga bisa
+            join trip ke Kanada, New Zealand, Afrika, Amerika Latin dll. Semangat terus buat bikin open trip ke
+            destinasi keceh badai lainnya yaa...</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/ajenk.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Ajenk</h4>
+              <p class="cust-profession">Karyawati</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Happy bareng Mayopentrip.</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/asrie.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Asrie</h4>
+              <p class="cust-profession">Traveler</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Sudah 3X liburan bareng mayopentrip Iran,China dan Rusia semua paketnya murah tapi bukan kaleng-kakengan
+            semua fasilitasnya nyaman.Next trip utk liburan bareng mayopentrip lagi dong.</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/dewi.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Dewi</h4>
+              <p class="cust-profession">Ibu rumah tangga</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Joint Mayopentrip pasti ketagihan, destinasi menarik dan complete (banyak), hotel berbintang, akrab sesama
+            peserta krn bisa masak bareng, berbagi perbekalan, pasti puas...buruan cobain..enjoy your trip and excited
+            experience</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/endang.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Endang</h4>
+              <p class="cust-profession">Pengusaha</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Selama perjalanan tour bersama uni, sangat menyenangkan, kekeluargaan, mengikuti keingginan, mendengarkan
+            dan masukan dr peserta tour , semua perjalanan menjadi berkesan dan uni sangat perhatian.Semoga uni sll
+            sehat dan sll bs memandu
+            kami tour keliling dunia Aamiin YRA love you uni may😘😘😘</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/ida.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Ida Farida</h4>
+              <p class="cust-profession">Notaris</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>pokoknya happy bisa puas kebeberapa tempat Indah yang kayanya kalau pake travel ternama bisa mahal banget
+            ... </p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/irma.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Irma</h4>
+              <p class="cust-profession">Ibu rumah tangga</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Smg lebih baik lagi ...sukses selalu!</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/liza.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Liza Irvan</h4>
+              <p class="cust-profession">Traveler</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Sdh 3x ikut mayopentrip.. So far semua berending happy..destinasi yg dipilih agak extraordinary..kluar dari
+            pakem umum yg biasa mementingkan efisiensi jarak dan waktu. Justru itu yg bikin keinginan kita terakomodasi.
+            Maju trs mayopentrip..insya Allah 2021 daftar lg...</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/marwati.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Marwati Widyamartana</h4>
+              <p class="cust-profession">Traveler</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Waktu ikut grup Jordan aqso palestina sangat berkesan dan kekeluargaan, fasilitasnya pun lengkap dan
+            memuaskan</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/maryani.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Maryani Mas'ud</h4>
+              <p class="cust-profession">Dokter</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Kapan nge-trip lagiii... Nyaman.....grup spt keluarga sendiri...flexible...tour leadernya kerennn....gabung
+            yukkk....</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/putiri.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Putiri Katili</h4>
+              <p class="cust-profession">Traveler</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Saya senang. Seperti sedang berlibur bersama keluarga. Semuanya akrab. Penginapan bagus. Makanan enak.
+            Destinasi lengkap dan berkualitas. Biaya murah. Jujur dan amanah.</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/sidiq.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Sidiq Gandi</h4>
+              <p class="cust-profession">ASN</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Maju teruuuss</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/tetri.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Tettri Vivi</h4>
+              <p class="cust-profession">Notaris</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Lanjutkan.. Sy ikut 3 kali NZ, Rusia dan Canada.. pokoknya keren</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/yani.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Yani</h4>
+              <p class="cust-profession">Traveler</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title"><img src="https://img.icons8.com/ultraviolet/40/000000/quote-left.png"></h4>
+        <div class="template-demo">
+          <p>Yuk ceritain kesan perjalanan kamu bersama mayopentrip ke berbagai penjuru dunia!</p>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-sm-2"> <img class="profile-pic" src="img/testimonial/orang.png">
+          </div>
+          <div class="col-sm-10">
+            <div class="profile">
+              <h4 class="cust-name">Kamu</h4>
+              <p class="cust-profession">Traveler</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="https://widgets.skyscanner.net/widget-server/js/loader.js" async></script>
+@endsection
